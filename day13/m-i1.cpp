58f1e444968b2cb8914d2e7403d3e6e68bb5a634
Jun Hyun Bang <flintintoe@gmail.com>
2019-07-17 13:22:41 +0900

    Update m-i1.cpp

--- day13/m-i1.cpp
+++ day13/m-i1.cpp
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void tri(int*, int*, int*);
+
+int main(int argc, char *argv[]) {
+	int base, height, area = 0;
+	
+	printf("삼각형 밑변과 높이를 입력");
+	scanf("%d%d", &base, &height);
+	
+	int *pbase = &base;
+	int *pheight = &height;
+	int *parea = &area;;
+	
+	tri(pbase, pheight, parea); 
+	
+	printf("삼각형 넓이: %d", area);
+	return 0;
+}
+
+//int tri(int base, int height) {
+//	return base * height / 2;
+//}
+
+void tri(int *base, int *height, int *area) {
+	*area = (*base) * (*height) / 2;
+}
