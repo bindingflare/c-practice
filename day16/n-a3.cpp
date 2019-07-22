f5813683cb79899f5a25cd49b435b110efd26497
Jun Hyun Bang <flintintoe@gmail.com>
2019-07-22 13:34:30 +0900

    Update n-a3.cpp

--- day16/n-a3.cpp
+++ day16/n-a3.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+// *c 활용은 c 언어에 배웠고, 밑 코드는 c++ 에서 할수있음 (c++ 에는 둘다 쓰기 가능) 
+
 void KK (int &c) { // a 변수의 주소를 다른 별명으로 선언 
 	c = 200; // 10 번지 값에 20을 넣어라 
 }
